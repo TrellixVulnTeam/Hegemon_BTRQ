a8e5aa5409d891039de2486efdd6dc30edd75dde
Oliver Tucher <oliver.tucher@gmail.com>
2021-12-24 17:54:41 -0800

    Updated post_init

--- src/perl_subprocess.py
+++ src/perl_subprocess.py
@@ -5,48 +5,52 @@ from io import StringIO
 
 @dataclass
 class PerlFiles:
-    expr_file: str
+    expr: pd.DataFrame
+    filebase: str
 
-    def thr(self) -> pd.DataFrame:
+    def __post_init__(self):
+        self.expr.to_csv(self.filebase+"-expr.txt", sep="\t", index=False)
+        self.thr().to_csv(self.filebase+"-thr.txt", sep="\t", index=False)
+
+    def thr(self) -> pd.DataFrame: # parses expr file    
         result = subprocess.run(["perl", "-I", 
                                 "/booleanfs/sahoo/scripts", 
                                 "/booleanfs/sahoo/scripts/absoluteInfo.pl", "thr", 
-                                self.expr_file, 
+                                self.filebase+"-expr.txt", 
                                 "2", "70000", "0.5"], 
                                 capture_output=True,
                                 text=True)
         thr_df = pd.read_csv(StringIO(result.stdout))
         return thr_df
 
-    def info(self) -> pd.DataFrame:
+    def info(self) -> pd.DataFrame: # parses thr file
         result = subprocess.run(["perl", "-I",
                                 "/booleanfs/sahoo/scripts",
                                 "/booleanfs/sahoo/scripts/hegemonutils.pl",
                                 "Info",
-                                self.expr_file[:-9]], 
+                                self.filebase], 
                                 capture_output=True,
                                 text=True)
         info_df = pd.read_csv(StringIO(result.stdout))
         return info_df
 
-
-    def vinfo(self) -> pd.DataFrame: #parses thr file
+    def vinfo(self) -> pd.DataFrame: # parses thr file
         result = subprocess.run(["perl", "-I",
                                 "/booleanfs/sahoo/scripts",
                                 "/booleanfs/sahoo/scripts/hegemonutils.pl",
                                 "VInfo",
-                                self.expr_file[:-9]], 
+                                self.filebase], 
                                 capture_output=True,
                                 text=True)
         vinfo_df = pd.read_csv(StringIO(result.stdout))
         return vinfo_df
         
-    def bv(self) -> pd.DataFrame: #parses thr file
+    def bv(self) -> pd.DataFrame: # parses thr file
         result = subprocess.run(["perl", "-I",
                                 "/booleanfs/sahoo/scripts",
                                 "/booleanfs/sahoo/scripts/hegemonutils.pl",
                                 "bv",
-                                self.expr_file[:-9]], 
+                                self.filebase], 
                                 capture_output=True,
                                 text=True)
         bv_df = pd.read_csv(StringIO(result.stdout))

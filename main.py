5a98c42b0771cf81ca410b607f4292f13570175a
Oliver Tucher <oliver.tucher@gmail.com>
2021-12-25 00:13:52 -0800

    updated main call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import pandas as pd
+import os
 
 from src.preprocessing.gse import GSE
 from src.hegemon import Hegemon
@@ -8,6 +10,16 @@ parser.add_argument(
     "-g",
     "--gse",
     help="NCBI GEO GSE Accession ID to parse",
+)
+parser.add_argument(
+    "-e",
+    "--expr",
+    help="expr text file tab seperated",
+)
+parser.add_argument(
+    "-s",
+    "--survival",
+    help="survival text file tab seperated",
 )
 args = parser.parse_args()
 
@@ -19,6 +31,18 @@ def parse_gse(accessionID: str) -> None:
         filebase = f"{accessionID}-{gpl_name}"
         Hegemon(expr=expr, survival=survival, filebase=filebase)
 
+def parse_expr_survival(expr: str, survival: str) -> None:
+    expr = pd.read_csv(expr, sep="\t")
+    survival = pd.read_csv(survival, sep="\t")
+    filebase = os.path.basename(expr)[:-9]
+    Hegemon(expr=expr, survival=survival, filebase=filebase)
+
+def main():
+    if args.gse != None:
+        parse_gse(args.gse)
+    elif args.expr != None and args.survival != None:
+        parse_expr_survival(args.expr, args.survival)
+
 if __name__ == "__main__":
-    gpls = parse_gse(args.gse)
+    main()
     
